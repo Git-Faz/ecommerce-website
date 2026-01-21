2022d8775c0971764d154ebdea568e000b4d54b2
Git-Faz <fahad.blr@icloud.com>
2026-01-22 01:05:14 +0530

    product ui updated

--- frontend/src/components/product/ProductCard.tsx
+++ frontend/src/components/product/ProductCard.tsx
@@ -1,13 +1,12 @@
 import {
     Card,
     CardAction,
-    CardDescription,
     CardContent,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 import type { JSX } from "react";
+import { ShoppingCartIcon as Cart } from "lucide-react";
 
 interface ProductCard {
     img: {
@@ -16,23 +15,32 @@ interface ProductCard {
     },
     name: string,
     price: number;
-    onClick: () => void
+    onClick: () => void,
+    onBtnClick: () => void
 }
 
-const ProductCard = ({ img, name, price, onClick }: ProductCard): JSX.Element => {
+const ProductCard = ({ img, name, price, onClick, onBtnClick }: ProductCard): JSX.Element => {
+
     return (
-        <Card onClick={onClick}
-        className="m-2 px-0 w-56 pt-0 pb-2 gap-3 min-h-56 flex flex-col bg-neutral-800 border-0 rounded-lg cursor-pointer text-white"
-        >
-            <div className="">
-                <img src={img.link} alt={img.alt} className="w-full max-w-56 h-fit p-0 m-0 gap-0 border rounded-t-lg border-neutral-800"></img>
+        <Card id="productCard">
+
+            <div onClick={onClick}>
+                <img src={img.link} alt={img.alt} className="w-full max-w-56 h-fit p-0 m-0 gap-0 border-b border-purple-200 rounded-t-lg"></img>
             </div>
-            <CardHeader className="gap-0 px-3 text-center">
+
+            <CardHeader className="gap-0 px-3 text-center" onClick={onClick}>
                 <CardTitle>{name}</CardTitle>
             </CardHeader>
             <CardContent className="gap-0 text-center" >
                 <p>₹{price}</p>
             </CardContent>
+            <CardAction className="self-center">
+                <button
+                    onClick={onBtnClick}
+                    className="flex justify-between p-2 min-w-fit w-30 my-1 mx-auto text-center bg-purple-300 text-neutral-800 hover:text-white text-sm rounded-md transition-all duration-200 ease-in hover:bg-purple-500 focus:bg-purple-500">
+                    Add to cart <Cart size={"2 0px"}/>
+                </button>
+            </CardAction>
         </Card>
     )
 }
