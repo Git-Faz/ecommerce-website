8ac28d58a580bdb396a4f6b6ebe11fb735386118
Git-Faz <fahad.blr@icloud.com>
2026-02-08 15:33:12 +0530

    updated redux to manage auth lifecycle

--- frontend/src/store/slices/authSlice.ts
+++ frontend/src/store/slices/authSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, type PayloadAction } from "@reduxjs/toolkit";
+import { login as loginApi } from "@/api/authApi";
 
 export interface User {
     id: number;
@@ -9,6 +10,8 @@ export interface User {
 export interface AuthState {
     user: User | null;
     token: string | null;
+    status: "idle" | "loading" | "error";
+    error: string | null
 }
 
 const loadInitialState = (): AuthState => {
@@ -20,8 +23,10 @@ const loadInitialState = (): AuthState => {
             const payload = JSON.parse(atob(token.split(".")[1]));
             if (payload.exp * 1000 > Date.now()) {
                 return {
-                    token,
                     user: JSON.parse(user),
+                    token,
+                    status: "idle",
+                    error: null,
                 };
             }
         } catch {
@@ -30,29 +35,62 @@ const loadInitialState = (): AuthState => {
         }
     }
 
-    return { user: null, token: null };
+    return { user: null, token: null, status:'idle', error:null };
 };
 
 const initialState: AuthState = loadInitialState();
+
+export const login = createAsyncThunk(
+    'auth/login', //type
+    async(credentials:{username: string, password: string}, {rejectWithValue}) => { //async func / payloadCreator
+        try{
+            const {data} = await loginApi(credentials)
+            return data;
+        } catch (err: any) {
+            if(err.response?.status === 401 || err.response?.status === 403){
+                return rejectWithValue("Invalid username or password")
+            }
+            return rejectWithValue("Login failed")
+        }
+    }
+)
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        loginSuccess(state, action: PayloadAction<{ user: User; token: string }>) {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-            localStorage.setItem("token", action.payload.token);
-            localStorage.setItem("user", JSON.stringify(action.payload.user));
-        },
         logout(state) {
             state.user = null;
             state.token = null;
+            state.status = "idle";
+            state.error = null;
             localStorage.removeItem("token");
             localStorage.removeItem("user");
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(login.pending, (state) => {
+                state.status = "loading"
+                state.error = null
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                const user = action.payload.user;
+                const token = action.payload.token;
+
+                state.user = user;
+                state.token = token;
+                state.status = "idle"
+
+                localStorage.setItem("user", JSON.stringify(user))
+                localStorage.setItem("token", token)
+            })
+            .addCase(login.rejected, (state, action)=> {
+                state.status = "error"
+                state.error = (action.payload as string) ?? "something went wrong";
+            })
+    }
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { logout } = authSlice.actions;
 export default authSlice.reducer;
