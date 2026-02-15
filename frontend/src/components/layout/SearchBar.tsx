9a3fae42b8100d8a0f98c709c7ad6a6dd040ca27
Git-Faz <fahad.blr@icloud.com>
2026-02-15 21:08:23 +0530

    fixed searchbar

--- frontend/src/components/layout/SearchBar.tsx
+++ frontend/src/components/layout/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, type JSX } from "react"
+import { useNavigate } from "react-router-dom"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import { SearchIcon } from "lucide-react"
@@ -15,6 +16,7 @@ export default function SearchBar({ value = "", onSearch, className }: SearchBar
 
   const [searchParams] = useSearchParams();
   const urlQuery = searchParams.get("name") || "";
+  const navigate = useNavigate()
     
     const [query, setQuery] = useState(urlQuery || value);
 
@@ -25,18 +27,27 @@ export default function SearchBar({ value = "", onSearch, className }: SearchBar
 
   useEffect(() => {
     const handler = setTimeout(() => {
-      if (query.trim().length === 0 || query.trim().length >= 5) {
-        onSearch(query.trim())
-      }
-    }, 300);
+       const trimmedQuery = query.trim();
+            
+            // If query is empty and we're on the products page, go home
+            if (trimmedQuery.length === 0 && window.location.pathname.startsWith('/products')) {
+                navigate('/');
+                return;
+            }
+            
+            // Only call onSearch if query is valid
+            if (trimmedQuery.length >= 5) {
+                onSearch(trimmedQuery);
+            }
+        }, 300);
     return () => {
       clearTimeout(handler)
     }
-  }, [query, onSearch])
+  }, [query, onSearch, navigate])
 
 
   return (
-    <div className={cn("flex flex-row w-lg min-w-xs max-w-3xl flex-2 m-2 p-2", className)}>
+    <div className={cn("flex flex-row w-lg min-w-xs max-w-3xl flex-2 m-1 p-2", className)}>
       <Input
         placeholder="Search..."
         value={query}
